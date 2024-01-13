de69ad44828074beef9cc186ea211d5806379bfd
wwxx-star <58853590+wwxx-star@users.noreply.github.com>
2024-01-14 07:18:43 +0800

    Update ipq60xx.mk

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -9,3 +9,16 @@ define Device/linksys_mr7350
 	DEVICE_PACKAGES := kmod-leds-pca963x
 endef
 TARGET_DEVICES += linksys_mr7350
+
+define Device/linksys_mr7350-plus
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Linksys
+	DEVICE_MODEL := MR7350-plus
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	SOC := ipq6018
+	DEVICE_PACKAGES := kmod-leds-pca963x
+endef
+TARGET_DEVICES += linksys_mr7350-plus
+
